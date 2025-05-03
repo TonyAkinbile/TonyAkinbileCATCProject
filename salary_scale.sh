cf32c8a51f0e145a1b89ec1c6848e1418d8b9302
Tony Akinbile <tonyakinbile92gmail.com>
2025-05-03 07:56:59 +0000

    Q2.4: added user input part to script

--- salary_scale.sh
+++ salary_scale.sh
@@ -11,3 +11,21 @@
 # If manager → use double increment
 # Use a loop to calculate and display each year's salary
 
+
+
+# ============================
+# USER INPUT
+# ============================
+
+echo "Enter the starting salary:"
+read salary
+
+echo "Enter the number of points on the salary scale:"
+read points
+
+echo "Enter the yearly salary increment:"
+read increment
+
+echo "Is the employee a manager? (yes/no):"
+read manager
+
