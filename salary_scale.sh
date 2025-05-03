bb91004d0287d2881ec3631329ac4250d18202c3
Tony Akinbile <tonyakinbile92gmail.com>
2025-05-03 07:47:06 +0000

    Q2.4: added pseudo code to script

---
+++ salary_scale.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ============================
+# PSEUDO CODE
+# ============================
+# Ask user for:
+#  - Starting salary
+#  - Number of points on the salary scale
+#  - Salary increment
+#  - If the employee is a manager
+# If manager → use double increment
+# Use a loop to calculate and display each year's salary
+
